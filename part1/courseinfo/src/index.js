19379d44a5c2c1583ded59f2fbdb9d93331d4c8e
Andrew Plotner <bluebandit21@gmail.com>
2019-08-13 03:35:57 -0400

    1.4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -15,30 +15,30 @@ const Content = (props) => {
   }
 const Total = (props) => {
     let sum=0;
-    for(let i=0;i<props.exercise_counts.length;i++){
-        sum+=props.exercise_counts[i];
-    }
-    return  (<p>Number of exercises {sum}</p>)
+    props.parts.forEach(element => sum+=element.exercises);
+    return (<p>Number of exercises {sum}</p>)
 }
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1,part2,part3]} />
-      <Total exercise_counts={[part1.exercises,part2.exercises,part3.exercises]} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
