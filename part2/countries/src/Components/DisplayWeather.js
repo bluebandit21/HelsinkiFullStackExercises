fc40a9bb7447a90e5cb5325b19c9270e78d7fe11
Andrew Plotner <bluebandit21@gmail.com>
2019-12-17 11:05:25 -0500

    Improved 'countries'

--- part2/countries/src/Components/DisplayWeather.js
+++ part2/countries/src/Components/DisplayWeather.js
@@ -3,17 +3,20 @@ import axios from 'axios'
 import renderHTML from 'react-render-html';
 
 const DisplayWeather = ({country}) => {
-    const OPENWEATHERMAP_API_KEY="1e62eeb5997264304f629b59f8bbb34f"
+    const OPEN_WEATHER_MAP_API_KEY="1e62eeb5997264304f629b59f8bbb34f"
     const [queryResponse, setQueryResponse] = useState('')
+    const [lastQuery, setLastQuery] = useState('')
     const queryWeather = (country) => {
+        if(lastQuery === country.name) return
+        setLastQuery(country.name)
         axios
-            .request(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital},${country.cioc}&mode=html&units=imperial&appid=${OPENWEATHERMAP_API_KEY}`)
+            .request(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital},${country.cioc}&mode=html&units=imperial&appid=${OPEN_WEATHER_MAP_API_KEY}`)
             .then(request => {
                 setQueryResponse(request.data)
             })
             .catch(error => {
                 console.log("Errror in API call, ",error)
-                setQueryResponse(<p>Weather Query Failed</p>)
+                setQueryResponse(`<p>Weather Query Failed</p>`)
             })
     }
     queryWeather(country)
